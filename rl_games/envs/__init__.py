4e1f6645872bc54bb7a02bdf3aaa2e2cb25f5fe4
KyleM73 <kyle.morgenstein@gmail.com>
2022-09-22 16:16:08 -0500

    added custom policies

--- rl_games/envs/__init__.py
+++ rl_games/envs/__init__.py
@@ -2,7 +2,11 @@
 
 from rl_games.envs.connect4_network import ConnectBuilder
 from rl_games.envs.test_network import TestNetBuilder
+from rl_games.envs.multi_input_network import Multi_Input_A2CBuilder
+from rl_games.envs.multi_input_network_replay import Multi_Input_Replay_A2CBuilder
 from rl_games.algos_torch import model_builder
 
 model_builder.register_network('connect4net', ConnectBuilder)
 model_builder.register_network('testnet', TestNetBuilder)
+model_builder.register_network('multi_input_net', Multi_Input_A2CBuilder)
+model_builder.register_network('multi_input_net_replay', Multi_Input_Replay_A2CBuilder)
